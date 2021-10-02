3418c4a8fb0ccf61d59d17d24122fc5ed0648749
xujiantong@mac <314783087@qq.com>
2021-10-02 14:31:29 +0800

    只引入 JS 文件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+const globModules = import.meta.glob("./glob/*.js")
+console.log(globModules)
+Object.entries(globModules).forEach(([k,v]) => {
+    console.log(k+ ":" + v)
+    v().then(m => console.log(k + ":" ,m.default))
+})
+
 createApp(App).mount('#app')
