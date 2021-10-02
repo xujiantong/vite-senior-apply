0a04927c3f0dc20524d21d70dcab266a944b8303
xujiantong@mac <314783087@qq.com>
2021-10-02 14:33:18 +0800

    只引入 指定正则

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const globModules = import.meta.glob("./glob/*.json")
+const globModules = import.meta.glob("./glob/*-[0-9].js")
 console.log(globModules)
 Object.entries(globModules).forEach(([k,v]) => {
     console.log(k+ ":" + v)
