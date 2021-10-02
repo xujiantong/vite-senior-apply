d677ebb2cacde0dee5536f097ed27b63014c0450
xujiantong@mac <314783087@qq.com>
2021-10-02 14:06:56 +0800

    实现单文件 HMR 热更新

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+export function render () {
+  document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+}
+
+render()
+
+// 集成 HMR
+// vite build 时 import.meta.hot 是不存在的, 所以加开发环境的判断条件
+// 常规范式: 在开发环境中设置 HRM 热更新
+if (import.meta.hot) { 
+   // 接收热更新 
+   // newModule 对应的当前文件
+   import.meta.hot.accept((newModule) => {
+    newModule.render();
+   })
+}
